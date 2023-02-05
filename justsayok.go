2ccdaf8b9a46316fa01a0a305a4f36d0edff5c58
Asfand Qazi <asfand@cloudership.com>
2023-02-05 13:05:34 +0000

    Starts and starting errors are now logged using 12factor strategy

--- justsayok.go
+++ justsayok.go
@@ -3,14 +3,20 @@ package main
 import (
 	"net/http"
 	"os"
+	"fmt"
 )
 
 func main() {
 	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {})
-	var port, ok = os.LookupEnv("PORT")
+	port, ok := os.LookupEnv("PORT")
 	if !ok {
 		port = "3000"
 	}
-	println(`{"event": "started", "message": "Server started on port ` + port + `"}`)
-	http.ListenAndServe(":"+port, nil)
+	fmt.Printf(`{"event": "started", "message": "Server started on port %s"}%s`, port, "\n")
+
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		fmt.Println(fmt.Errorf(`{"event": "error_starting", "message": "%v"}`, err))
+		os.Exit(1)
+	}
 }
